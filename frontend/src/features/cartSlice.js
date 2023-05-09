3a487f29b59174c594516fa2742ba2885e110f39
veetsok <94634131+veetsok@users.noreply.github.com>
2023-05-09 18:20:24 +0300

    Add files via upload

--- frontend/src/features/cartSlice.js
+++ frontend/src/features/cartSlice.js
@@ -2,9 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
 
 const initialState = {
-  cartItems: localStorage.getItem("cartItems")
-    ? JSON.parse(localStorage.getItem("cartItems"))
-    : [],
+  cartItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [],
   cartTotalQuantity: 0,
   cartTotalAmount: 0,
 };
@@ -14,22 +12,17 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart(state, action) {
-      const itemIndex = state.cartItems.findIndex(
-        (item) => item.id === action.payload.id
-      );
+      const itemIndex = state.cartItems.findIndex((item) => item.id === action.payload.id);
 
       if (itemIndex >= 0) {
         state.cartItems[itemIndex].cartQuantity += 1;
-        toast.info(
-          `increased ${state.cartItems[itemIndex].title} cart quantity`,
-          {
-            position: "bottom-left",
-          }
-        );
+        toast.info(`Вы добавили еще один ${state.cartItems[itemIndex].title}`, {
+          position: "bottom-left",
+        });
       } else {
         const tempProduct = { ...action.payload, cartQuantity: 1 };
         state.cartItems.push(tempProduct);
-        toast.success(`${action.payload.title} added to cart`, {
+        toast.success(`Вы добавили в корзину ${action.payload.title}`, {
           position: "bottom-left",
         });
       }
@@ -37,35 +30,29 @@ const cartSlice = createSlice({
     },
 
     removeFromCart(state, action) {
-      const nextCartItems = state.cartItems.filter(
-        (cartItem) => cartItem.id !== action.payload.id
-      );
+      const nextCartItems = state.cartItems.filter((cartItem) => cartItem.id !== action.payload.id);
 
       state.cartItems = nextCartItems;
       localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
-      toast.error(`${action.payload.title} remove from cart`, {
+      toast.error(`Вы удалили из корзины ${action.payload.title}`, {
         position: "bottom-left",
       });
     },
 
     decreaseCart(state, action) {
-      const itemIndex = state.cartItems.findIndex(
-        (cartItem) => cartItem.id === action.payload.id
-      );
+      const itemIndex = state.cartItems.findIndex((cartItem) => cartItem.id === action.payload.id);
 
       if (state.cartItems[itemIndex].cartQuantity > 1) {
         state.cartItems[itemIndex].cartQuantity -= 1;
-        toast.info(`Decreased ${action.payload.title} cart quantitycart`, {
+        toast.info(`Вы уменьшили ${action.payload.title} в корзине`, {
           position: "bottom-left",
         });
       } else if (state.cartItems[itemIndex].cartQuantity === 1) {
-        const nextCartItems = state.cartItems.filter(
-          (cartItem) => cartItem.id !== action.payload.id
-        );
+        const nextCartItems = state.cartItems.filter((cartItem) => cartItem.id !== action.payload.id);
 
         state.cartItems = nextCartItems;
 
-        toast.error(`${action.payload.title} remove from cart`, {
+        toast.error(`Вы удалили из корзины ${action.payload.title}`, {
           position: "bottom-left",
         });
       }
@@ -94,7 +81,6 @@ const cartSlice = createSlice({
   },
 });
 
-export const { addToCart, removeFromCart, decreaseCart, getTotals } =
-  cartSlice.actions;
+export const { addToCart, removeFromCart, decreaseCart, getTotals } = cartSlice.actions;
 
 export default cartSlice.reducer;
